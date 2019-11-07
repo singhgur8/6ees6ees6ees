7fa8fe000d520b20a8562fa312dd9a0a2304864f
Gurjot Singh <imasingh@ucdavis.edu>
2019-11-06 19:22:48 -0800

    finished

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
+class ForagerBee extends Grub{
   // TODO..
+  constructor(){
+    super();
+    this.age = 10,
+    this.color = 'yellow',
+    this.job = 'find pollen',
+    this.canFly = true,
+    this.treasureChest = [];
+  }
+  forage(string){
+    this.treasureChest.push(string);
+  }
+
 };
